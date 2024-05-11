f32ba8110a72a9076e786cffac6a16e5e3626fc2
Ukasyah <hi.ukasyah@gmail.com>
2024-05-11 20:14:26 +0700

    add initial components

---
+++ src/lib/preset.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ['Reddit Sans', 'sans-serif'],
+				sans: ['Manrope', 'sans-serif']
+			},
+			fontSize: {
+				sm: '0.85rem',
+				base: '0.9425rem',
+				'2xl': '1.535rem'
+			}
+		}
+	},
+	plugins: []
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<svelte:head>
+	<title>Komuna UI</title>
+</svelte:head>
+
+<div class="absolute h-full w-full flex flex-col items-center justify-center text-center">
+	<h1 class="text-3xl leading-snug">Komuna UI</h1>
+	<p class="dimmed mt-2">Open source Svelte component library for Komuna web apps.</p>
+</div>
