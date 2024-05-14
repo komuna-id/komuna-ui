3dad7b4ffa86b2ee2a5d4d5e604d326b451addba
Ukasyah <hi.ukasyah@gmail.com>
2024-05-15 05:29:01 +0700

    make md button bigger

--- src/lib/components/navigation/button.svelte
+++ src/lib/components/navigation/button.svelte
@@ -23,9 +23,9 @@
 		'font-semibold tracking-wide',
 		'relative flex items-center justify-center whitespace-nowrap',
 		size === 'sm' && 'px-3.5 h-9 text-sm rounded-md',
-		size === 'md' && 'px-4 h-10 text-base rounded-md',
+		size === 'md' && 'px-5 h-11 text-base rounded-md',
 		square && size === 'sm' && 'px-0 w-9',
-		square && size === 'md' && 'px-0 w-10',
+		square && size === 'md' && 'px-0 w-11',
 		variant === 'default' &&
 			'bg-white text-neutral-800/80 border border-neutral-200 shadow-sm hover:bg-neutral-50',
 		variant === 'primary' && 'bg-blue-600/90 text-neutral-50 hover:bg-blue-600',
